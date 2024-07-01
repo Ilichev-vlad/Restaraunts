6b9e706e7f212a92935f5111601a2fcc98f4bde9
Ilichev-vlad <vlad.ilichev999@gmail.com>
2024-07-01 16:05:37 +0300

    initial

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { restaurants } from '../materials/mock';
+
+ReactDOM.createRoot(document.getElementById('root')).render()
